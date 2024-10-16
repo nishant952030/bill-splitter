3b7c353f3cd7fc8d021eb4aa14460543869813b6
NISHANT SHARMA <nishant735123@gmail.com>
2024-10-16 22:36:18 +0530

    changed constant

--- client/src/components/constant.js
+++ client/src/components/constant.js
@@ -1,14 +1,14 @@
 
-// const userRoute = 'https://bill-server-dutu.onrender.com/user'; const searchRoute = 'https://bill-server-dutu.onrender.com/search';const expenseRoute = 'https://bill-server-dutu.onrender.com/expense'; 
+const userRoute = 'https://bill-server-dutu.onrender.com/user'; const searchRoute = 'https://bill-server-dutu.onrender.com/search'; const expenseRoute = 'https://bill-server-dutu.onrender.com/expense'; const groupRoute = 'https://bill-server-dutu.onrender.com/group'; 
 
  
  
 
- const userRoute = 'http://localhost:8000/user';
- const expenseRoute = 'http://localhost:8000/expense'; 
- const searchRoute = 'http://localhost:8000/search';  
- const groupRoute = 'http://localhost:8000/group';  
+//  const userRoute = 'http://localhost:8000/user';const expenseRoute = 'http://localhost:8000/expense'; const searchRoute = 'http://localhost:8000/search';  const groupRoute = 'http://localhost:8000/group';  
+ 
+ 
 
+ 
 // Export them
 module.exports = {
     userRoute,
