138732727895e9cbf7124f78e96ad0806d7bc994
NISHANT SHARMA <nishant735123@gmail.com>
2024-10-26 01:13:54 +0530

    notification route problem solved

--- client/src/components/constant.js
+++ client/src/components/constant.js
@@ -1,5 +1,5 @@
 
-const userRoute = 'https://bill-server-dutu.onrender.com/user'; const searchRoute = 'https://bill-server-dutu.onrender.com/search'; const expenseRoute = 'https://bill-server-dutu.onrender.com/expense'; const groupRoute = 'https://bill-server-dutu.onrender.com/group'; const notificationRoute = 'http://localhost:8000/notification'
+const userRoute = 'https://bill-server-dutu.onrender.com/user'; const searchRoute = 'https://bill-server-dutu.onrender.com/search'; const expenseRoute = 'https://bill-server-dutu.onrender.com/expense'; const groupRoute = 'https://bill-server-dutu.onrender.com/group'; const notificationRoute = 'https://bill-server-dutu.onrender.com/notification'
 
 // const userRoute = 'http://localhost:8000/user'; const expenseRoute = 'http://localhost:8000/expense'; const searchRoute = 'http://localhost:8000/search'; const groupRoute = 'http://localhost:8000/group'; const notificationRoute = 'http://localhost:8000/notification'
 
